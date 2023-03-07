ed801ea9bc37aea4a3cdc5fecc4c3741cbf7907a
Muyale Okoyo <annmuyale@gmail.com>
2023-03-07 21:33:15 +0300

    corrected

--- strings.js
+++ strings.js
@@ -1,6 +1,7 @@
 let b = "beautiful";
-console.log(b[5],b[6],b[7],b[8]);
+console.log(b[0],b[2],b[3],b[4]);
 String.prototype.insert = function(index, string){
+
     if (index > 0)
     {
         return food.substring(0,index) + string + food.substring(index, food.length);
@@ -10,24 +11,38 @@ String.prototype.insert = function(index, string){
 var food = "I was feeling hungry today";
 food = food.insert(5," eat");
 console.log([food]);
+
+
 const story = "the quick brown fox jumps over the lazy dog";
-let count = (story.match(/the/g) || []).length;
+let count = (story.match(/the/r) || []).length;
 console.log("count",count);
-let tell = (story.match(/fox/g) || []).length;
+let tell = (story.match(/fox/r) || []).length;
+
+
+
 console.log("tell",tell);
 const string1 = "We are now going to school";
 const word = "now";
 console.log(`The word "${word}" ${string1.includes(word)? "is" : "is not"} in the sentence`);
+
+
 const string2 = "The child was sitting on the table before it fell";
 const text = "sitting";
 console.log(`The word "${text}" ${string2.includes(text)? "is" : "is not"} in the sentence`);
+
+
+
+
 const string3 = "wonderful";
 console.log(string3.toUpperCase());
-const string4 = "amazing";
-const string5 = "BEneath"
-console.log(string4.toLowerCase(),string5.toLowerCase());
-const string6 = "A beautiful wedding"
-const string7 = string6.split(" ")
-.map(w=>w[0].toUpperCase () + w.substring(1).toLowerCase())
-.join(" ");
-console.log(string7);
+
+
+
+
+
+
+
+
+
+
+
